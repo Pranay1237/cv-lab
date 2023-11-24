4fd7f01474b888f581f6ff3278e5fca2013cd13e
Pranay1237 <mudhirajpranay30@gmail.com>
2023-11-24 09:29:42 +0530

    initial commit

--- week10/colorSpace.py
+++ week10/colorSpace.py
@@ -5,11 +5,11 @@ import matplotlib.pyplot as plt
 image = cv2.imread('img2.jpg')
 
 cmy_image = 255 - image
-cmy = cv2.cvtColor(image, cv2.COLOR_BGR2LAB)
+lab = cv2.cvtColor(image, cv2.COLOR_BGR2LAB)
 hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
-images = [image, cmy_image, cmy, hsv_image]
-titles = ['Original Image', 'CMY Image', 'CMY', 'HSV Image']
+images = [image, cmy_image, lab, hsv_image]
+titles = ['Original Image', 'CMY Image', 'LAB', 'HSV Image']
 
 for i in range(4):
     plt.subplot(2, 2, i+1)
--- week8/hist_equi.py
+++ week8/hist_equi.py
@@ -5,14 +5,13 @@ image = cv2.imread('img2.jpg', cv2.IMREAD_GRAYSCALE)
 
 equalized_image = cv2.equalizeHist(image)
 
-plt.subplot(1, 2, 1)
-plt.imshow(image, cmap='gray')
-plt.title('Original Image')
-plt.xticks([]), plt.yticks([])
+images = [image, equalized_image]
+titles = ['Original Image', 'Equalized Image']
 
-plt.subplot(1, 2, 2)
-plt.imshow(equalized_image, cmap='gray')
-plt.title('Equalized Image')
-plt.xticks([]), plt.yticks([])
+for i in range(2):
+    plt.subplot(1, 2, i+1)
+    plt.imshow(images[i], cmap='gray')
+    plt.title(titles[i])
+    plt.xticks([]), plt.yticks([])
 
 plt.show()
