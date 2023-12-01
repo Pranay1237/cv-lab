a4cd58dd66c876e75699ad9bb8befbf43c9c6935
Pranay1237 <mudhirajpranay30@gmail.com>
2023-12-01 12:10:06 +0530

    initial commit

--- week7/sift.py
+++ week7/sift.py
@@ -2,14 +2,14 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-img = cv2.imread('img1.jpg')
+img = cv2.imread('shapes.jpg')
 img = cv2.resize(img, (650, 600))
 img1 = img.copy()
 
 g = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 sift_ob = cv2.SIFT_create()
 kp = sift_ob.detect(g, None)
-img = cv2.drawKeypoints(g, kp, img)
+img = cv2.drawKeypoints(g, kp, img, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 img = cv2.resize(img, (650, 600))
 
 images = [img1, img]
