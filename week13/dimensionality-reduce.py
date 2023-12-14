c94eff238e49846101d4a2eb886005148756962b
Pranay1237 <mudhirajpranay30@gmail.com>
2023-12-14 12:05:02 +0530

    initial commit

--- week13/dimensionality-reduce.py
+++ week13/dimensionality-reduce.py
@@ -1,33 +1,25 @@
-import cv2
 import numpy as np
-from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
-
-# Load the image
-img = cv2.imread('flower.jpg', cv2.IMREAD_GRAYSCALE)
-
-# Flatten the image to a 1D array
-img_flattened = img.flatten()
-
-# Apply PCA
-pca = PCA(n_components=0.95)  # You can adjust the explained variance threshold
-img_pca = pca.fit_transform(img_flattened.reshape(1, -1))
-
-# Inverse transform to get the reduced image
-img_reduced = pca.inverse_transform(img_pca)
-
-# Reshape the flattened image to its original shape
-img_reduced = img_reduced.reshape(img.shape)
-
-# Display the original and reduced images
-plt.figure(figsize=(8, 4))
-
-plt.subplot(1, 2, 1)
-plt.imshow(img, cmap='gray')
-plt.title('Original Image')
-
-plt.subplot(1, 2, 2)
-plt.imshow(img_reduced, cmap='gray')
-plt.title('Reduced Dimensionality using PCA')
-
+from sklearn.datasets import load_iris
+from sklearn.decomposition import PCA
+iris = load_iris()
+X = iris.data
+Y = iris.target
+pca = PCA(n_components= 2)
+X_pca = pca.fit_transform(X)
+plt.figure(figsize=(8,6))
+for target,target_name in enumerate(iris.target_names):
+    plt.scatter(X[Y == target,0],X[Y == target,1],label = target_name)
+plt.xlabel('Feature 1')
+plt.ylabel('Feature 2')
+plt.title('original.iris Dataset(20)')
+plt.legend()
+plt.show()
+plt.figure(figsize=(8,6))
+for target,target_name in enumerate(iris.target_names):
+    plt.scatter(X_pca[Y == target,0],X_pca[Y == target,1],label = target_name)
+plt.xlabel('principal component 1')
+plt.ylabel('principal component 2')
+plt.title('Iris Dataset after PCA(2D)')
+plt.legend()
 plt.show()
